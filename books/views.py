2ba9e56e4a8222caaa05ab597ecd954c10b04a9f
Luan Pimenta <luan.pimenta@academico.ifpb.edu.br>
2024-10-31 11:41:35 -0300

    add book

--- books/views.py
+++ books/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BookForm
+from django.contrib import messages
 
-# Create your views here.
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.quantity_available = book.quantity_total
+            book.save()
+            messages.success(request, "Livro adicionado com sucesso!")
+            return redirect('add_book')
+        else:
+            messages.error(request, "Erro ao adicionar livro! Verifique os campos abaixo.")
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
--- setup/urls.py
+++ setup/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', redirect_to_login, name='root_redirect'),
     path('', include('users.urls')),
+    path('', include('books.urls')),
 ]
